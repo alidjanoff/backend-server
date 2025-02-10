c874810b289e5e9eff1f50a39fabc621e9c763b2
alidjanoff <alidjanoff@hotmail.com>
2025-02-10 10:48:58 +0400

    mongoose routes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const { getCart, addToCart } = require('../controllers/cartController');
+// const express = require('express');
+// const router = express.Router();
+// const { getCart, addToCart } = require('../controllers/cartController');
 
-router.get('/cart/:userId', getCart);
-router.post('/cart', addToCart);
+// router.get('/cart/:userId', getCart);
+// router.post('/cart', addToCart);
+
+// module.exports = router;
+
+const express = require("express");
+const router = express.Router();
+const { getCart, addToCart } = require("../controllers/cartControllers");
+
+// Kullanıcının sepetini getirme
+router.get("/cart/:userId", getCart);
+
+// Sepete ürün ekleme
+router.post("/cart", addToCart);
 
 module.exports = router;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,20 +1,42 @@
-const express = require('express');
+// const express = require('express');
+// const router = express.Router();
+// const { getProducts, getProductById, addProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+
+// // Get all products (any user can view products)
+// router.get('/products', getProducts);
+
+// // Get a single product by ID (any user can view a product)
+// router.get('/products/:id', getProductById);
+
+// // Add a new product (only admin can add)
+// router.post('/products', addProduct);
+
+// // Update an existing product (only admin can update)
+// router.put('/products/:id', updateProduct);  // PUT için updateProduct fonksiyonu kullanılıyor
+
+// // Delete a product (only admin can delete)
+// router.delete('/products/:id', deleteProduct);
+
+// module.exports = router;
+
+
+const express = require("express");
 const router = express.Router();
-const { getProducts, getProductById, addProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const { getProducts, getProductById, addProduct, updateProduct, deleteProduct } = require("../controllers/productController");
 
-// Get all products (any user can view products)
-router.get('/products', getProducts);
+// Tüm ürünleri getir (herkes erişebilir)
+router.get("/products", getProducts);
 
-// Get a single product by ID (any user can view a product)
-router.get('/products/:id', getProductById);
+// Belirli bir ürünü ID ile getir (herkes erişebilir)
+router.get("/products/:id", getProductById);
 
-// Add a new product (only admin can add)
-router.post('/products', addProduct);
+// Yeni bir ürün ekle (sadece admin ekleyebilir)
+router.post("/products", addProduct);
 
-// Update an existing product (only admin can update)
-router.put('/products/:id', updateProduct);  // PUT için updateProduct fonksiyonu kullanılıyor
+// Mevcut bir ürünü güncelle (sadece admin güncelleyebilir)
+router.put("/products/:id", updateProduct);
 
-// Delete a product (only admin can delete)
-router.delete('/products/:id', deleteProduct);
+// Bir ürünü sil (sadece admin silebilir)
+router.delete("/products/:id", deleteProduct);
 
 module.exports = router;
